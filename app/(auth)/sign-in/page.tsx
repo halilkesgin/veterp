2572bc6532f3119e0c450b033371b54b5880476e
Halil <halillkesgin@gmail.com>
2024-03-04 02:37:16 +0300

    dep test3

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,25 +1,28 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { SignInForm } from "./_components/sign-in-form"
 import { PawPrint } from "lucide-react"
+import { Suspense } from "react"
 
 const SignInPage = () => {
     return (
-        <Card className="lg:w-[500px] w-auto">
-            <CardHeader>
-                <div className="flex items-center justify-center">
-                    <PawPrint className="h-10 w-10" />
-                </div>
-                <CardTitle className="font-bold">
-                    veterp
-                </CardTitle>
-                <CardDescription>
-                    Hoşgeldiniz, gerekli alanları doldurarak giriş yapabilirsiniz.
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <SignInForm />
-            </CardContent>
-        </Card>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Card className="lg:w-[500px] w-auto">
+        <CardHeader>
+          <div className="flex items-center justify-center">
+            <PawPrint className="h-10 w-10" />
+          </div>
+          <CardTitle className="font-bold">
+            veterp
+          </CardTitle>
+          <CardDescription>
+            Hoşgeldiniz, gerekli alanları doldurarak giriş yapabilirsiniz.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+      </Card>
+    </Suspense>
     )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { SignUpForm } from "./_components/sign-up-form"
 import { PawPrint } from "lucide-react"
+import { Suspense } from "react"
 
 const SignUpPage = () => {
     return (
+        <Suspense fallback={<div>Loading...</div>}>
+
         <Card className="lg:w-[500px] w-auto">
             <CardHeader>
                 <div className="flex items-center justify-center">
@@ -20,6 +23,8 @@ const SignUpPage = () => {
                 <SignUpForm />
             </CardContent>
         </Card>
+        </Suspense>
+
     )
 }
 
